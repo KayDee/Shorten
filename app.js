cb502497b7ea6e338b99f8b4c1a5f559b99319df
kaydee <kaydey287@gmail.com>
2018-12-23 22:37:54 +0530

    Still broken, trying things

--- app.js
+++ app.js
@@ -2,12 +2,26 @@ let fs = require('fs')
 let path = require('path')
 let express = require('express')
 let app = express()
+let router = express.Router()
 
-app.use(express.static(path.join('__dirname', 'src')))
+// let index = require('./src/ind.html')
 
-app.use('/', function(req, res){
-  res.render('index')
+app.use(express.static(path.join(__dirname, 'src')))
+
+// app.use('/', function(req, res){
+  // res.render('index')
+  // console.log(path.join(__dirname, '/index.html'))
+  // res.sendFile(path.join(__dirname, 'src/index.html'))
+  // console.log(res)
+// })
+
+// app.use('/', index)
+
+router.get('/', function(req, res){
+  res.send(' Yo yo')
 })
+
+app.use('/', router)
 
 app.listen(4000, function(){
   console.log('Hearing')
